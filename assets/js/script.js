262639c5c1859da4590212b1f9323e4282112a8b
NukaGrizz <nukagrizz@gmail.com>
2021-07-11 00:28:28 -0400

    add reduction to time when answer wrong and questions three through five

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,19 @@ var buttonTwo = document.getElementById('btn-Two');
 var buttonThree = document.getElementById('btn-Three');
 var buttonFour = document.getElementById('btn-Four');
 var answerResult = document.getElementById('qResult');
-var answer = false;
 
 function startTest() {
-    var timeLeft = 100;
+    var timeLeft = 80;
 
     var countdown = function() {
         console.log(timeLeft);
         timeLeft--;
         timer.textContent = 'Time: ' + timeLeft;
+        if (timeLeft === 0) {
+            alert("timesup");
+            clearInterval(timeInterval);
+            fail();
+        };
     };
 
     var timeInterval = setInterval(countdown, 1000);
@@ -25,7 +29,7 @@ function startTest() {
         
         para.classList.add("hide"); 
         titleQ.classList.add("questionhead")
-        titleQ.textContent = "Commonly used data types do not include ____________.";
+        titleQ.textContent = "Commonly used data types DO NOT include ____________.";
         startBtn.classList.add("hide");
         buttonOne.classList.remove("hide");
         buttonOne.textContent = "1. strings";
@@ -43,12 +47,13 @@ function startTest() {
     };
 
     function secFalse() {
-        answer = false;
+        answerResult.textContent = "Wrong!";
+        timeLeft = timeLeft - 20;
         secondQuestion();
     };
 
     function secTrue() {
-        answer = true;
+        answerResult.textContent = "Correct!";
         secondQuestion();
     };
 
@@ -58,7 +63,8 @@ function startTest() {
         buttonTwo.textContent = "2. curly brackets";
         buttonThree.textContent = "3. parenthesis";
         buttonFour.textContent = "4. square brackets";
-
+        answerResult.classList.remove("hide");
+        
         buttonOne.onclick = thrFalse;
         buttonTwo.onclick = thrFalse;
         buttonThree.onclick = thrTrue;
@@ -66,26 +72,86 @@ function startTest() {
     };
 
     function thrFalse() {
-        answer = false;
+        answerResult.textContent = "Wrong!";
+        timeLeft = timeLeft - 20;
         thirdQuestion();
     };
 
     function thrTrue() {
-        answer = true;
+        answerResult.textContent = "Correct!";
         thirdQuestion();
     };
 
     function thirdQuestion() {
-        titleQ.textContent = "The condition in a if / else statment is enclosed with ____________.";
-        buttonOne.textContent = "1. quotes";
-        buttonTwo.textContent = "2. curly brackets";
-        buttonThree.textContent = "3. parenthesis";
-        buttonFour.textContent = "4. square brackets";
+        titleQ.textContent = "Arrays in Javascript can be used to store ____________.";
+        buttonOne.textContent = "1. numbers and strings";
+        buttonTwo.textContent = "2. other arrays";
+        buttonThree.textContent = "3. booleans";
+        buttonFour.textContent = "4. all of the above";
 
         buttonOne.onclick = forFalse;
         buttonTwo.onclick = forFalse;
-        buttonThree.onclick = forTrue;
-        buttonFour.onclick = forFalse;
+        buttonThree.onclick = forFalse;
+        buttonFour.onclick = forTrue;
+    };
+
+    function forFalse() {
+        answerResult.textContent = "Wrong!";
+        timeLeft = timeLeft - 20;
+        forthQuestion();
+    };
+
+    function forTrue() {
+        answerResult.textContent = "Correct!";
+        forthQuestion();
+    };
+
+    function forthQuestion() {
+        titleQ.textContent = "String values must be closed within ____________ when being assigned to variables.";
+        buttonOne.textContent = "1. commas";
+        buttonTwo.textContent = "2. curly brackets";
+        buttonThree.textContent = "3. quotes";
+        buttonFour.textContent = "4. parenthesis";
+
+        buttonOne.onclick = fivFalse;
+        buttonTwo.onclick = fivFalse;
+        buttonThree.onclick = fivTrue;
+        buttonFour.onclick = fivFalse;
+    };
+
+    function fivFalse() {
+        answerResult.textContent = "Wrong!";
+        timeLeft = timeLeft - 20;
+        fifthQuestion();
+    };
+
+    function fivTrue() {
+        answerResult.textContent = "Correct!";
+        fifthQuestion();
+    };
+
+    function fifthQuestion() {
+        titleQ.textContent = "a very useful tool used during development and debugging for printing content to the debugger is:";
+        buttonOne.textContent = "1. Javascript";
+        buttonTwo.textContent = "2. terminal/bash";
+        buttonThree.textContent = "3. for loops";
+        buttonFour.textContent = "4. console.log";
+
+        buttonOne.onclick = sixFalse;
+        buttonTwo.onclick = sixFalse;
+        buttonThree.onclick = sixTrue;
+        buttonFour.onclick = sixFalse;
+
+        function sixFalse() {
+            answerResult.textContent = "Wrong!";
+            timeLeft = timeLeft - 20;
+            saveScore();
+        };
+    
+        function sixTrue() {
+            answerResult.textContent = "Correct!";
+            saveScore();
+        };
     };
 };
 
